1e41da2431ba305bea8f36a4ae61345177566842
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-21 18:04:41 +0300

    adds auth setup for frontend

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,22 +1,58 @@
-import axios from "axios"
+// import axios from "axios"
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_APP_API_URL
+// })
+
+// api.interceptors.request.use((config)=>{
+//     return config
+// })
+
+// api.interceptors.response.use(
+//     (response)=> response,
+//     (error)=>{
+//         if(error.response != undefined && error.response.status === 401){
+
+//         }
+
+//         return Promise.reject(error)
+//     }
+// )
+
+// export default api
+
+
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL
+    baseURL: import.meta.env.VITE_APP_BASE_URL
 })
 
-api.interceptors.request.use((config)=>{
+api.interceptors.request.use((config) =>{
     return config
 })
 
-api.interceptors.response.use(
-    (response)=> response,
-    (error)=>{
-        if(error.response != undefined && error.response.status === 401){
+// api.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//         if (error.response != undefined && error.response.status === 401){
 
-        }
+//         }
+//         return Promise.reject(error)
+//     }
+// )
 
-        return Promise.reject(error)
+// Add a request interceptor to include the token in headers
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  });
+  
+//   export default api;
 
 export default api
